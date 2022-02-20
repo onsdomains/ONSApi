de5d7275f1aab3c6296746c33f9a52213387c717
Abolfazl Arab <mranoncoder@gmail.com>
2022-02-20 22:01:04 +0100

    First

---
+++ README.md
@@ -0,0 +1,9 @@
+# ChemistryDataAPI
+
+This is a public JSON based API,which gives information about Chemical Elements.Ideal to use in any of your Chemistry related projects,feel free to make some amazing code project.
+
+Base URL:https://chemistrydata.herokuapp.com/
+
+API documentation:https://drive.google.com/file/d/10hFZmmxUmn2gPiG6mlpsI-XWlAMjf035/view?usp=drivesdk
+
+
---
+++ index.js
@@ -0,0 +1,122 @@
+var fs=require('fs');
+
+var dataAbi=fs.readFileSync('contract.json');
+const abi = JSON.parse(dataAbi);
+
+var ControllerdataAbi=fs.readFileSync('contract.json');
+const ControllerAbi = JSON.parse(ControllerdataAbi);
+
+
+const express = require("express");
+const app = express();
+const cors=require('cors');
+const Web3 = require('web3');
+const provider = new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/ab93e43c332040f38f3fa436417d01ec");
+const web3 = new Web3(provider);
+var Jimp = require("jimp");
+
+
+web3.eth.getBlockNumber().then((result) => {
+	console.log("Latest Emerald Block is ",result);
+});
+
+const ONS = new web3.eth.Contract(abi, "0x56c94D0d1340572eFFb21ea9E88252513E1f4319");
+const controller = new web3.eth.Contract(ControllerAbi, "0x2bAf23a0F1C563792B106F6AB4d29D2Fe1d42aB4");
+
+app.listen(5000, () => console.log("Server Start at 5000 Port"));
+
+app.use(express.static('public'));
+app.use(cors());
+app.get('/token',alldata);
+function alldata(request,response)
+{
+    response.send(elements);
+}
+
+app.get('/token/:token/',getToken);
+async function getToken(request,response)
+{
+	var tokenID = request.params.token;
+
+	const checkAvailable = await ONS.methods.available(tokenID).call() ;
+
+	if(checkAvailable === false)
+	{
+		const getName = await ONS.methods.getNamebyID(tokenID).call() ;
+		const getExpireDate = await ONS.methods.nameExpires(tokenID).call();
+	//	const getExpireDate = await ONS.methods.controller(tokenID).call();
+		var date = {
+			is_normalized: true,
+			name:getName,
+			symbol:"ONS",
+			description:getName + " is an ONS name.",
+			image:"",
+			external_url:"https://ons.money/" +  tokenID,
+			attributes:[
+				{
+					trait_type: "Created Date",
+					display_type:"date",
+					value: "Common"
+				},{
+					trait_type: "Length",
+					display_type:"number",
+					value: 6
+				},{
+					trait_type: "Registration Date",
+					display_type:"date",
+					value: 1639711359000
+				},{
+					trait_type: "Expiration Date",
+					display_type:"date",
+					value: +getExpireDate
+				}
+			]
+
+
+		};
+		
+	}
+	else
+	{
+		var date={
+			status:"The token not exist"
+		}
+	}
+	response.send(date);
+
+}
+
+app.get('/image/:image/',getImage);
+async function getImage(request, response) {
+	var tokenID = request.params.image
+	const checkAvailable = await ONS.methods.available(tokenID).call()
+
+	if (checkAvailable === false) {
+		const getName = await ONS.methods.getNamebyID(tokenID).call()
+		var fileName = 'test.png'
+		var imageCaption = getName
+		var loadedImage
+
+		Jimp.read(fileName)
+			.then(function (image) {
+				loadedImage = image
+				return Jimp.loadFont(Jimp.FONT_SANS_16_BLACK)
+			})
+			.then(async function (font) {
+				const b64 = await loadedImage
+					.print(font, 10, 10, imageCaption.toString())
+					.getBase64Async(Jimp.MIME_PNG, (res) => console.log(res))
+				response.writeHead(200, {
+					'Content-Type': 'image/png',
+				})
+				var base64Data = b64.replace(/^data:image\/png;base64,/, '')
+				var img = Buffer.from(base64Data, 'base64')
+
+				response.end(img)
+			})
+			.catch(function (err) {
+				console.error(err)
+			})
+	}
+}
+
