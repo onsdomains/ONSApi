a26d3deae816c3debb7edd0958c9520ab419f367
Abolfazl Arab <mranoncoder@gmail.com>
2022-02-26 10:16:59 +0100

    Changes the URI data! Changes the Font size

--- index.js
+++ index.js
@@ -11,40 +11,28 @@ const express = require("express");
 const app = express();
 const cors=require('cors');
 const Web3 = require('web3');
-const provider = new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/ab93e43c332040f38f3fa436417d01ec");
+const provider = new Web3.providers.HttpProvider("https://emerald.oasis.dev");
 const web3 = new Web3(provider);
 var Jimp = require("jimp");
 
-
-web3.eth.getBlockNumber().then((result) => {
-	console.log("Latest Emerald Block is ",result);
-});
-
-const ONS = new web3.eth.Contract(abi, "0x56c94D0d1340572eFFb21ea9E88252513E1f4319");
-const controller = new web3.eth.Contract(ControllerAbi, "0x2bAf23a0F1C563792B106F6AB4d29D2Fe1d42aB4");
+const ONS = new web3.eth.Contract(abi, "0xFFb32987c496364cd752cB196bFBE01D8D0D7e48");
+const controller = new web3.eth.Contract(ControllerAbi, "0xBd295ab4BC7Cb4BDf4B7558B9d3f18C5Baee0AA5");
 
 app.listen(5000, () => console.log("Server Start at 5000 Port"));
 
 app.use(express.static('public'));
 app.use(cors());
-app.get('/token',alldata);
-function alldata(request,response)
-{
-    response.send(elements);
-}
 
 app.get('/token/:token/',getToken);
 async function getToken(request,response)
 {
 	var tokenID = request.params.token;
-
 	const checkAvailable = await ONS.methods.available(tokenID).call() ;
-
 	if(checkAvailable === false)
 	{
 		const getName = await ONS.methods.getNamebyID(tokenID).call() ;
 		const getExpireDate = await ONS.methods.nameExpires(tokenID).call();
-	//	const getExpireDate = await ONS.methods.controller(tokenID).call();
+		var stringSplit = getName.split(".");
 		var date = {
 			is_normalized: true,
 			name:getName,
@@ -56,16 +44,12 @@ async function getToken(request,response)
 				{
 					trait_type: "Domain",
 					display_type:"string",
-					value: ".ons"
+					value: stringSplit[1]
 				},
 				{
 					trait_type: "Length",
 					display_type:"number",
-					value: 6
-				},{
-					trait_type: "Registration Date",
-					display_type:"date",
-					value: 1639711359000
+					value: stringSplit[0].length
 				},{
 					trait_type: "Expiration Date",
 					display_type:"date",
@@ -94,6 +78,8 @@ async function getImage(request, response) {
 
 	if (checkAvailable === false) {
 		const getName = await ONS.methods.getNamebyID(tokenID).call()
+		var stringSplit = getName.split(".");
+
 		var fileName = 'test.png'
 		var imageCaption = getName
 		var loadedImage
@@ -101,11 +87,17 @@ async function getImage(request, response) {
 		Jimp.read(fileName)
 			.then(function (image) {
 				loadedImage = image
-				return Jimp.loadFont(Jimp.FONT_SANS_16_BLACK)
+				if (stringSplit[0].length >= 3){
+					return Jimp.loadFont(Jimp.FONT_SANS_128_BLACK)
+				}else if (stringSplit[0].length >= 15){
+					return Jimp.loadFont(Jimp.FONT_SANS_64_BLACK)
+				}else{
+					return Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
+				}
 			})
 			.then(async function (font) {
 				const b64 = await loadedImage
-					.print(font, 10, 10, imageCaption.toString())
+					.print(font, 80, 800, imageCaption.toString())
 					.getBase64Async(Jimp.MIME_PNG, (res) => console.log(res))
 				response.writeHead(200, {
 					'Content-Type': 'image/png',
